f3f57e306068ad4c3bf6b87904019c608d69791e
izeye <izeye@naver.com>
2015-01-28 01:26:47 +0900

    Test application.properties.

--- src/main/java/test/Application.java
+++ src/main/java/test/Application.java
@@ -1,5 +1,7 @@
 package test;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +9,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * Created by izeye on 15. 1. 28..
  */
 @SpringBootApplication
-public class Application {
+public class Application implements CommandLineRunner {
+
+	@Value("${something}")
+	private String something;
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println(something);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
