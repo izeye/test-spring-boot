ec40a591b49aa167058fb0b126dd53909b422f36
izeye <izeye@naver.com>
2015-01-28 01:20:50 +0900

    Initial commit

---
+++ src/main/java/test/Application.java
@@ -0,0 +1,16 @@
+package test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by izeye on 15. 1. 28..
+ */
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
